dedd2aa23207d70a897a92dc59b8836ff56800ad
Manjay-webdev <161963231+manjaycoder@users.noreply.github.com>
2025-07-26 22:02:57 +0530

    Revert "db models created"

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,16 +1,11 @@
 import express from "express"
-import { ENV } from "./config/env.js";
-import { connectDB } from "./config/db.js";
 
 
 const app=express();
-connectDB()
-app.get("/",(req,res)=>{
-    res.send("<h1>Manjay</h1>")
-})
 
 
 
-app.listen(ENV.PORT,()=>{
+
+app.listen(5001,()=>{
     console.log("Server is running ")
 })
