610076edad81bb010a2fedf7ae7206e12c5c57da
manjaycoder <manjay.verma.coder@gmail.com>
2025-07-30 19:18:24 +0530

    user routes added

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,15 +1,17 @@
 import express from "express";
 import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
-import userRoutes from "./routes/user.route.js"
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 import { connectDB } from "./config/db.js";
-import {ENV} from "./config/env.js"
+import { ENV } from "./config/env.js";
 const app = express();
 app.use(cors());
 
 app.use(clerkMiddleware());
 app.get("/", (req, res) => res.send("Hello from server"));
-app.use("/api/users",userRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 const startServer = async () => {
   try {
     await connectDB();
